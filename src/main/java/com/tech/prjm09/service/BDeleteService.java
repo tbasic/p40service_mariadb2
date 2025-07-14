c840b8d4e777f076e5ad883bf024d95767fbad21
tbasic <tbasicapp@gmail.com>
2025-07-14 12:07:06 +0900

    init

---
+++ src/main/java/com/tech/prjm09/service/BDeleteService.java
@@ -0,0 +1,29 @@
+package com.tech.prjm09.service;
+
+import java.util.Map;
+
+import org.springframework.ui.Model;
+
+import com.tech.prjm09.dao.IDao;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class BDeleteService implements BServiceInter{
+	private IDao iDao;
+	
+	public BDeleteService(IDao iDao) {
+		this.iDao=iDao;
+	}
+	@Override
+	public void execute(Model model) {
+		System.out.println(">>>BDeleteService");
+		Map<String, Object> map=model.asMap();
+		HttpServletRequest request=
+				(HttpServletRequest) map.get("request");
+				
+		String bid=request.getParameter("bid");	
+		iDao.delete(bid);
+		
+	}
+
+}
